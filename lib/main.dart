5b93640550aff64191ddaa0b3fb1648f66242911
worada.sir <worada.sir@axonstech.com>
2024-04-02 00:32:34 +0700

    reset main to roll dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
-import 'package:first_app/quiz_app/quiz.dart';
+import 'package:first_app/roll_dice_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Quiz());
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          colors: [
+            Color.fromARGB(255, 71, 26, 149),
+            Color.fromARGB(255, 119, 83, 181)
+          ],
+        ),
+      ),
+    ),
+  );
 }
