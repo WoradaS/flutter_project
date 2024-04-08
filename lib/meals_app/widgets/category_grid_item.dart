ac9b84aa041d9f3145fb78158f22b17ee8a6efb1
worada.sir <worada.sir@axonstech.com>
2024-04-08 22:02:54 +0700

    [MEALS APP]: Building Multi-Screen Apps & Navigating Between Screens

---
+++ lib/meals_app/widgets/category_grid_item.dart
@@ -0,0 +1,41 @@
+import 'package:first_app/meals_app/models/category.dart';
+import 'package:flutter/material.dart';
+
+class CategoryGrigItem extends StatelessWidget {
+  const CategoryGrigItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
+
+  final Category category;
+  final void Function() onSelectCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onSelectCategory,
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.55),
+                category.color.withOpacity(0.9),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(16)),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      ),
+    );
+  }
+}
